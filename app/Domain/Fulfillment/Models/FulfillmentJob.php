9027984cabda088997cb7e5b3e44c8e80a43f4ba
Samer Isaa <samer.isaa1996@gmail.com>
2026-01-19 23:53:12 +0200

    up

--- app/Domain/Fulfillment/Models/FulfillmentJob.php
+++ app/Domain/Fulfillment/Models/FulfillmentJob.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Domain\Fulfillment\Models;
 
+use App\Domain\Orders\Models\Order;
 use App\Domain\Orders\Models\OrderItem;
 use App\Domain\Fulfillment\Models\FulfillmentEvent;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -16,6 +17,7 @@ class FulfillmentJob extends Model
     use HasFactory;
 
     protected $fillable = [
+        'order_id',
         'order_item_id',
         'fulfillment_provider_id',
         'payload',
@@ -40,6 +42,10 @@ class FulfillmentJob extends Model
     public function orderItem(): BelongsTo
     {
         return $this->belongsTo(OrderItem::class);
+    }
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
     }
 
     public function attempts(): HasMany
