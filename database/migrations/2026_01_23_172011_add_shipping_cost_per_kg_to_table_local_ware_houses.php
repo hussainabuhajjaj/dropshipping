0d7bead0f1123454bc8cedd73ce53bbab88c7d47
Hussain abu hajjaj <hussain.abuhajjaj993@gmail.com>
2026-01-23 18:24:45 +0100

    feat: add shipping cost fields to local ware houses table

---
+++ database/migrations/2026_01_23_172011_add_shipping_cost_per_kg_to_table_local_ware_houses.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('table_local_ware_houses', function (Blueprint $table) {
+            $table->string('shipping_company_name')->nullable()->after('name');
+            $table->string('shipping_method')->nullable()->after('shipping_company_name');
+            $table->float('shipping_min_charge', 10, 2)->default(0)->after('shipping_method');
+            $table->float('shipping_cost_per_kg', 10, 2)->default(0)->after('shipping_min_charge');
+            $table->float('shipping_base_cost', 10, 2)->default(0)->after('shipping_cost_per_kg');
+            $table->float('shipping_additional_cost', 10, 2)->default(0)->after('shipping_base_cost');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('table_local_ware_houses', function (Blueprint $table) {
+            //
+        });
+    }
+};
