b79eb140dbcb2cb1c718638f5085aac0c9eb52e4
Hussain abu hajjaj <hussain.abuhajjaj993@gmail.com>
2026-05-09 22:05:47 +0200

    Update services.php

--- config/services.php
+++ config/services.php
@@ -161,7 +161,7 @@ return [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
         'redirect' => env('FACEBOOK_REDIRECT_URI'),
-        'pixel_id' => env('FACEBOOK_PIXEL_ID', '1025735920629525'),
+        'pixel_id' => env('FACEBOOK_PIXEL_ID'),
     ],
 
     'apple' => [
